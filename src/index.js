963f82c09a7b57bf173d7b665c2b9c92c8159c26
Rafael Camargo Epifanio <rafael.epifanio@meta.com.br>
2020-05-30 20:04:20 -0300

    teste'

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = express()
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  return res.json({ message: 'Test' })
+  return res.json({ message: "Test" })
 })
 
 app.listen(3333, () => console.log('Server running on port 3333'))
