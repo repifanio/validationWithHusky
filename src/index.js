1fa7ddda49f6695f5f5017db041053f45d698613
Rafael Camargo Epifanio <rafael.epifanio@meta.com.br>
2020-05-30 23:53:57 -0300

    Ok

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = express()
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  return res.json({ message: 'Test de lint' })
+  return res.json({ message: 'Test de lint e tests com postman' })
 })
 
 app.listen(3333, () => console.log('Server running on port 3333'))
