834960a23197f2a4a151cb537d714e1c4676209c
Rafael Epifanio <rafael.cepifanio@gmail.com>
2020-05-31 00:25:28 -0300

    teste

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = express()
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  return res.json({ message: "Tests de lint e tests com postmsgit statan" })
+  return res.json({ message: "Tests de lint e testds com postmsgit statan" })
 })
 
 app.listen(3333, () => console.log('Server running on port 3333'))
